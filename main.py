bb060d61ebd0f67fba1e55f9847f9d049557a482
Rik de Goede <rik.dgd@gmail.com>
2024-11-15 00:46:03 +0100

    Add flask to the project and add a jinja2 demo page.

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+from flask import Flask
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader = FileSystemLoader('templates'))
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/found-vulns")
+def found_vulns_page():
+    template = env.get_template('results-view.html')
+    page_content = template.render(custom_element='<h1>epic developer time</h1>')
+    return page_content
 
 
-if __name__ == '__main__':
+
+def send_request():
     r = requests.get("https://example.com/")
     content = r.text
 
